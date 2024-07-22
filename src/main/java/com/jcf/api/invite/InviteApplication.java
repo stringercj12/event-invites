ab73a3ece7454fa0dc29c1e140240213b95507a8
Jefferson Ferreira <jefferson.ferreira@mjv.com.br>
2024-07-22 11:41:19 -0300

    Initial Commit

---
+++ src/main/java/com/jcf/api/invite/InviteApplication.java
@@ -0,0 +1,14 @@
+package com.jcf.api.invite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InviteApplication.class, args);
+        System.out.println("\n Aplicação Rodando ... \n");
+    }
+
+}
---
+++ src/test/java/com/jcf/api/invite/InviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcf.api.invite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
